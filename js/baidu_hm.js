e24658200edf127d13d0d514c8a6318bb98eee7c
xushiwei <465515386@qq.com>
2017-08-07 17:03:30 +0800

    update

---
+++ js/baidu_hm.js
@@ -0,0 +1,7 @@
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?a67593acb45112d4d9822fb1b84c4469";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
